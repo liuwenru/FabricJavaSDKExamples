de76bb8f3399f86f3b16874b5ee76d49ca5f8a66
ijarvis <ijarvis@sina.com>
2017-12-12 18:58:24 +0800

    add configure

---
+++ src/test/java/ijarvis/intelliq/AppTest.java
@@ -0,0 +1,18 @@
+package ijarvis.intelliq;
+
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    private static Logger logger=Logger.getLogger(AppTest.class);
+    @Test
+    public void Setup(){
+        logger.debug("Fabric Test Start........");
+
+    }
+
+}
